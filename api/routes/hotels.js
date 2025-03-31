e7f9b09e206b0e431728b211ff38773c75022ead
Manjeet Singh <98460132+manjeetdeveloper@users.noreply.github.com>
2025-03-31 13:00:51 +0530

    Update Hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,22 +1,32 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import Hotel from '../models/Hotel.js';
-
+import express from "express";
+import mongoose from "mongoose";
+import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
 
-
 //CREATE
 router.post("/", async (req, res) => {
+  const newHotel = new Hotel(req.body);
 
-    const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(error);
+  }
+});
 
-    try {
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    } catch (err) {
-        res.status(500).json(error);
-    }
-})
-
+//Update
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(error);
+  }
+});
 export default router;
