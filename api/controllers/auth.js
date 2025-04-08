72ecefbaf1d96611e7fc315f1882d878e97c355a
Manjeet Singh <98460132+manjeetdeveloper@users.noreply.github.com>
2025-04-09 02:28:11 +0530

    Register function is Successfully WOrking :- commit No12 check with Screenshot

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,16 +1,25 @@
-import User from "../models/User.js"
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
-export const register = async (req,res,next)=>{
-    try{
-        const newUser = new User({
-            username:req.body.username,
-            email:req.body.email,
-            password:req.body.password, 
-        })
+export const register = async (req, res, next) => {
+  try {
 
-        await newUser.save()
-        res.status(201).send("User has been Created.")
-    }catch(err){
-        next(err)
-    }
-}
+    //integrate HAsh PAsswords ------------------------------
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    // Store hash in your password DB
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
+
+    await newUser.save();
+    res.status(201).send("User has been Created.");
+  } catch (err) {
+    next(err);
+  }
+};
+
+
